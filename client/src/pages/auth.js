9a7e2f8b01b0d2348b435c5cde53a60c43be4161
Leonardo Garcia <leogarcia258@gmail.com>
2023-04-07 01:35:56 -0700

    register form created

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -14,3 +14,36 @@ export const Auth = () => {
   );
 };
 
+const Login = () => {
+    return <div></div>;
+};
+
+const Register= () => {
+    const[username, setUsername] = useState("")
+    const[password, setPassword] = useState("")
+
+    return (
+        <div className="auth-container">
+          <form>
+            <h2> Register</h2>
+            <div className="form-group">
+                <label htmlFor="username">Username:</label>
+                <input
+                 type="text"
+                 id="username"
+                 value={username}
+                 onChage={(event) => setUsername(event.target.value) }></input>
+            </div>
+            <div className="form-group">
+                <label htmlFor="password">Password:</label>
+                <input
+                 type="text" 
+                 id="password"
+                 value={password}
+                 onChage={(event) => setPassword(event.target.value)}></input>
+            </div>
+          </form>
+
+        </div>
+    )
+}
