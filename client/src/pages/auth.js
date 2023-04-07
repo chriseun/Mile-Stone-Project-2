3dbdc88aed18f81a471db458d29f175df774087b
Leonardo Garcia <leogarcia258@gmail.com>
2023-04-07 00:52:05 -0700

    created auth.js file

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -1,3 +1,15 @@
+import React, { useState } from "react";
+//replacement for fetch API, easier to handle
+import axios from "axios";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
+
+
 export const Auth = () => {
-    return <div> Auth</div>
+  return (
+    <div className="auth">
+      <Login />
+      <Register />
+    </div>
+  );
 };
