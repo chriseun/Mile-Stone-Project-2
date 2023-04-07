dfb251548d23b0741e4574a851e42406a071f3fa
Chris Eun <eunchristopher@gmail.com>
2023-04-07 01:48:07 -0700

    added auth.js and create recipe.js

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -15,35 +15,94 @@ export const Auth = () => {
 };
 
 const Login = () => {
-    return <div></div>;
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+
+  const [_, setCookies] = useCookies(["access_token"]);
+
+
+
+  const navigate = useNavigate();
+
+  const onSubmit = async (event) => {
+    //preventDefault will prevent from refreshing the page
+    event.preventDefault();
+    try{
+      const response = await axios.post("http://localhost:3001/auth/login", {
+        username,
+        password
+      });
+
+      setCookies("access_token", response.data.token);
+      window.localStorage.setItem("userID", response.data.userID);
+      navigate("/");
+      // window.location.pathname ="/"
+    } catch(err) {
+      console.error(err);
+
+    }
+
+  }
+
+  return (
+      <Form
+        username={username}
+        setUsername={setUsername}
+        password={password}
+        setPassword={setPassword}
+        label="Login"
+        onSubmit={onSubmit}
+      />
+  );
 };
 
-const Register= () => {
-    const[username, setUsername] = useState("")
-    const[password, setPassword] = useState("")
+const Register = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    return (
-        <div className="auth-container">
-          <form>
-            <h2> Register</h2>
-            <div className="form-group">
-                <label htmlFor="username">Username:</label>
-                <input
-                 type="text"
-                 id="username"
-                 value={username}
-                 onChage={(event) => setUsername(event.target.value) }></input>
-            </div>
-            <div className="form-group">
-                <label htmlFor="password">Password:</label>
-                <input
-                 type="text" 
-                 id="password"
-                 value={password}
-                 onChage={(event) => setPassword(event.target.value)}></input>
-            </div>
-          </form>
+  const onSubmit = async (event) => {
+    //preventDefault will prevent from refreshing the page
+    event.preventDefault();
 
+    try {
+      await axios.post("http://localhost:3001/auth/register", { username, password });
+      alert("Registration Completed! Now login")
+    } catch (err) {
+      console.error(err);
+    }
+
+  };
+
+  return (
+      <Form
+        username={username}
+        setUsername={setUsername}
+        password={password}
+        setPassword={setPassword}
+        label="Register"
+        onSubmit={onSubmit}
+      />
+  );
+};
+
+const Form = ({ username, setUsername, password, setPassword, label, onSubmit }) => {
+  return (
+  <div className="auth-container">
+    <form onSubmit={onSubmit}>
+      <h2> {label} </h2>
+        <div className="form-group">
+          <label htmlFor="username"> Username: </label>
+          <input type="text" id="username" value={username} onChange={(event) => setUsername(event.target.value)}/>
         </div>
-    )
+        <div className="form-group">
+          <label htmlFor="password"> Password: </label>
+          <input type="password" id="password" value={password} onChange={(event) => setPassword(event.target.value)}/>
+        </div>
+
+        <button type="submit"> {label}</button>
+    </form>
+  </div>
+  );
 }
