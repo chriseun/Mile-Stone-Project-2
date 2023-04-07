8876cdcf2fee1067e98689a27d157ea829666fd3
Chris Eun <eunchristopher@gmail.com>
2023-04-06 20:18:35 -0700

    created router.post for registering

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -2,15 +2,30 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
+import { UserModel } from '../models/Users.js'
+
 
 const router = express.Router()
 
 //register route
-router.post("/register")
+router.post("/register", async(req, res) => {
+  //this is the body of the request
+  const { username, password } = req.body
+
+  //UserModel from './models.User.js'
+  //we are making a request to UserModel
+  //findOne() = function that is calling one user
+  //'username: username' = username
+  //.then .catch or async await
+  const user = await UserModel.findOne({ username });
+
+  //we are going to send back a json
+  res.json(user)
+})
 
 
 //login route
-router.post("login")
+router.post("/login")
 
 
 
